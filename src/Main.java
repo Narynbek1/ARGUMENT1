c88c852795f897c0876231579683518446740845
naryn <narynbeknurbekov@gmail.com>
2023-03-24 22:11:08 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+//      method(5);
+//      method1(12,23);
+//
+//    }
+//    static void method(int a){
+//        System.out.println(a);
+//    }
+//    static void method1(int a, int b){
+//        System.out.println(a+b);
+
+//
+//        System.out.println(method(1, 2, 3, 4, 5, 6, 7, 8, 9));
+//    }
+//
+//    static  int method( int...a) {
+//        int sum = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            sum = sum + a[i];
+//
+//        }
+//
+//       return sum; //Varargs колдонуп метод тузунуз. Методко канча аргумент берсениз ошонун суммасын "кайтарсын"
+
+//        method();
+//    }
+//
+//    static void method() {
+//        String[] s = new String[]{"String"};
+//            for (String value : s) {
+//                System.out.println("Мени сени жакшы кором " + value);
+//            }
+
+        String text = "My name is Naryn";
+        System.out.println(text);
+        String newtext = text.concat("temirlan");
+        System.out.println(text.concat(text+"temirlan"));
+        System.out.println(text);
+
+    }
+}
+
